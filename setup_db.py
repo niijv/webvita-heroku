17ba17105ad858c0fe984e2c1539fd1d179178df
niijv <elias.zeitfogel@gmail.com>
2014-06-13 19:25:58 +0200

    Setup fixed.

--- setup_db.py
+++ setup_db.py
@@ -2,7 +2,7 @@ from wv import *
 
 db.drop_all()
 db.create_all()
-admin = User('Dummy', pwd_context.encrypt('dummy'), 'dummy@email.com')
+admin = User('Dummy', pwd_context.encrypt('dummy'), 'dummy@dummy.com')
 db.session.add(admin)
 db.session.commit()
 
