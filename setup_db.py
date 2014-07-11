3cc2564633501b0ad9b7466ebaae1000c0d22e9b
niijv <elias.zeitfogel@gmail.com>
2014-07-11 16:21:35 +0200

    DP setup automatic

--- setup_db.py
+++ setup_db.py
@@ -1,8 +1,9 @@
 from wv import *
 
-db.drop_all()
-db.create_all()
-admin = User('Dummy', 'Mr. Dummy', pwd_context.encrypt('dummy'), 'dummy@dummy.com')
-db.session.add(admin)
-db.session.commit()
+def setup_database():
+    db.drop_all()
+    db.create_all()
+    admin = User('Dummy', 'Mr. Dummy', pwd_context.encrypt('dummy'), 'dummy@dummy.com')
+    db.session.add(admin)
+    db.session.commit()
 
